6e60597e8a5f3d53e4807bfc1deec1632e8f6a8d
Harshit Garg <gargh2120@gmail.com>
2026-03-11 23:47:31 +0530

    page fixed error !

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+import TodoForm from "@/components/AddtodoForm";
+import dbConnection from "@/lib/database";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  await dbConnection();
   return (
-    <div>HEllo world </div>
+    <div className="overflow-y-scroll  no-scrollbar">
+    <nav className="bg-indigo-600 text-white">
+      <div className="max-w-4xl mx-auto px-6 py-4 flex justify-between items-center">
+        <span className="text-lg font-semibold">Todo App</span>
+
+        <Link 
+          href="/todos"
+          className="bg-white text-indigo-600 px-4 py-1 rounded-md font-medium hover:bg-gray-100 transition"
+        >
+          Todos
+        </Link>
+      </div>
+    </nav>
+
+    <TodoForm/>
+
+    <footer className="w-full bg-gray-900 text-gray-300 py-4 mt-10">
+      <div className="max-w-5xl mx-auto text-center text-sm">
+        © {new Date().getFullYear()} Todo App • Built with Next.js + TanStack Query
+      </div>
+    </footer>
+    </div>
   );
 }

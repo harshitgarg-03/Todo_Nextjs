c289e3b348a95afcfed59ac5f4715615b1ac2774
Harshit Garg <gargh2120@gmail.com>
2026-03-11 13:24:33 +0530

    todoSchema defined

---
+++ models/todo.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: [100, "title can't exceed more than 100 words"],
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: [2000, "description can't exceed more than 2000 words"],
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+  },
+  { timestamps: true },
+);
+
+export const Todo = mongoose.models.Todo || mongoose.model("Todo", todoSchema);

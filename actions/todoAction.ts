b589195d6f36464cea8aa3f5479f22d1b924bcf0
Harshit Garg <gargh2120@gmail.com>
2026-03-11 20:07:56 +0530

    Create todo action defined !

---
+++ actions/todoAction.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import dbConnection from "@/lib/database";
+import { Todo } from "@/models/todo";
+import { todoSchemsProp } from "@/type";
+import { createTodoSchema } from "@/validation/todo";
+
+export const createTodoAction = async (data: todoSchemsProp) => {
+  const validatedata = createTodoSchema.parse(data);
+  if (!validatedata) {
+    return Response.json({
+      success: false,
+      message: "error to valiadte todos.!",
+      status: 404,
+    });
+  }
+  await dbConnection();
+  try {
+    const Receivedata = await Todo.create(validatedata);
+    console.log("Receivedata is :", Receivedata);
+
+    return Response.json({
+      success: true,
+      message: "success to make todo",
+      status: 200,
+    });
+  } catch (error: any) {
+    console.log("error is ", error);
+    return Response.json({
+      success: false,
+      message: "error to make todo",
+      status: 404,
+    });
+  }
+};

47f28111d0fa4dca3f1c47f2ec57e2a1d812f9fc
Harshit Garg <gargh2120@gmail.com>
2026-03-11 19:35:13 +0530

    tanstack query defined globally

--- components/provider/QuerProvider.tsx
+++ components/provider/QuerProvider.tsx
@@ -2,9 +2,14 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
-
+import { Toaster } from "sonner";
 export const QueryProvider = ({ children }: { children: ReactNode }) => {
   const [client] = useState(() => new QueryClient());
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <Toaster />
+    </QueryClientProvider>
+  );
 };

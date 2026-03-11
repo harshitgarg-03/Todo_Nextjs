775b081550034632fed531f80bc0b68c6f261ab2
Harshit Garg <gargh2120@gmail.com>
2026-03-11 13:28:41 +0530

    interface  defined

---
+++ type/index.d.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+interface todoSchemsProp extends mongoose.documents {
+    title: string,
+    description: string,
+    priority: string,
+    completed: boolean
+}

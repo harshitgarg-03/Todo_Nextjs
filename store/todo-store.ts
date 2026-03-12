5be2a08a794ccdc6bfe3f6a6c1e70ae057519d7c
Harshit Garg <gargh2120@gmail.com>
2026-03-12 12:54:56 +0530

    use store defined

--- store/todo-store.ts
+++ store/todo-store.ts
@@ -15,7 +15,34 @@ export const useTodoStore = create(
         todos: [todo, ...state.todo],
       })),
 
+    updateTodo: (id, updates) => 
+      set((state) => ({
+        todos: state.todos.map((todo) => (todo._id == id) ? {...todo, ...updates} : todo),
+      })),
+
+    removeTodo: (id) => {
+      set((state) => ({
+        todos: state.todos.filter((todo) => todo._id != id)
+      }))
+    },
+
     setFilter: (filter: string) => set({ filter }),
     setLoading: (isLoading: string) => set({ isLoading }),
+
+    filteredTodos: () => {
+      const { todos, filter } = get();
+
+      switch (filter) {
+        case "active":
+          return todos.filter((todo) => !todo.completed);
+        case "completed":
+          return todos.filter((todo) => todo.completed);
+        default: 
+          return todos;
+      };
+
+    },
+    completedCount: () => get().todos.filter((todo) => todo.completed).length,
+    activeCount: () => get().todos.filter((todo) => !todo.completed).length
   }), {name: "todo-store"}),
 );

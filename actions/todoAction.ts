088451612413ff1baf0dc4f5631e7f076a93cbf7
Harshit Garg <gargh2120@gmail.com>
2026-03-12 12:51:03 +0530

    create todo

--- actions/todoAction.ts
+++ actions/todoAction.ts
@@ -5,6 +5,7 @@ import { Todo } from "@/models/todo";
 import { todoSchemsProp } from "@/type";
 import { createTodoSchema } from "@/validation/todo";
 import { revalidatePath } from "next/cache";
+import { success } from "zod";
 
 export const createTodoAction = async (data: todoSchemsProp) => {
   const validatedata = createTodoSchema.parse(data);
@@ -42,7 +43,7 @@ export const getTodoAction = async () => {
     await dbConnection();
 
     const todos = await Todo.find({}).sort({ createdAt: -1 }).lean();
-
+    revalidatePath("/");
     return {
       success: true,
       message: "todo fetched success ",
@@ -54,5 +55,66 @@ export const getTodoAction = async () => {
       message: "error to fetch todo",
       status: 404,
     });
+  }
+};
+
+export const toggleTodos = async (id) => {
+  try {
+    await dbConnection();
+
+    const todo = await Todo.findById(id);
+
+    if (!todo) {
+      return {
+        success: false,
+        error: "Todo not found!",
+      };
+    }
+
+    todo.completed = !todo.completed;
+
+    await todo.save();
+
+    revalidatePath("/");
+
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(todo)),
+    };
+  } catch (error) {
+    console.log("error to toggle todo");
+
+    return {
+      success: false,
+      error: error,
+    };
+  }
+};
+
+export const deleteAction = async (id) => {
+  await dbConnection();
+  try {
+    const todo = await Todo.findByIdAndDelete(id);
+
+    if (!todo) {
+      return {
+        success: false,
+        error: "Todo not found!",
+      };
+    }
+
+    revalidatePath("/");
+
+    return {
+      success: true,
+      deletedData: JSON.parse(JSON.stringify(todo)),
+    };
+  } catch (error) {
+    console.log("error to delete todo");
+
+    return {
+      success: false,
+      error: error,
+    };
   }
 };

ea0811f2e90ba0c1379b4a4d28e9ad34031c24af
Harshit Garg <gargh2120@gmail.com>
2026-03-12 09:42:19 +0530

    create todo

--- hooks/use-create-todo.ts
+++ hooks/use-create-todo.ts
@@ -1,27 +1,47 @@
 import { createTodoSchema } from "@/validation/todo";
-import { createTodoAction } from "@/actions/todoAction";
+import { createTodoAction, getTodoAction } from "@/actions/todoAction";
 import { useTodoStore } from "@/store/todo-store";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { todoSchemsProp } from "@/type";
 
 
 export const todoKeys = {
     all: ["todos"],
-    lists: () => [...todoKeys, "list"]
+    lists: () => [...todoKeys.all, "list"]
 }
 
 export function useCreateTodo() {
     const queryClient = useQueryClient();
-
-    const addTodo = useTodoStore((state) => state.addTodo);
+    const addTodo = useTodoStore((state: any) => state.addTodo);
 
     return useMutation({
-        mutationFn: (data) => createTodoAction(data),
+        mutationFn: (data: todoSchemsProp) => createTodoAction(data),
         onSuccess: (result) => {
             if(result.success){
                 addTodo(result.data)
 
-                queryClient.invalidateQueries({queryKey: todoKeys.list()})
+                queryClient.invalidateQueries({
+                    queryKey: todoKeys.lists()
+                })
             }
+        }
+    })
+}
+
+export function useTodos(){
+    const setTodos = useTodoStore((state) => state.setTodos);
+
+    return useQuery({
+        queryKey: todoKeys.lists(),
+        queryFn: async () => {
+            const result = await getTodoAction();
+
+            if(result.success){
+                setTodos(result.data);
+                return result.data;
+            }
+
+            throw new Error(result.Error);
         }
     })
 }
